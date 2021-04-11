70a1d28c04843606a6d73d78f2060d1578d8e0e2
MightyBoolik <bkuchaev@gmail.com>
2021-04-11 15:58:36 +0500

    Added .gitignore, build.gradle

---
+++ src/test/java/tests/AndroidBrowserstackTest.java
@@ -0,0 +1,50 @@
+package tests;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class AndroidBrowserstackTest {
+
+    @Test
+    void goolePixelTest(String[] args) throws MalformedURLException, InterruptedException {
+
+        DesiredCapabilities caps = new DesiredCapabilities();
+
+        // Set your access credentials
+        caps.setCapability("browserstack.user", "bulat14");
+        caps.setCapability("browserstack.key", "xyd9yyz4hnUbpV7QfF7e");
+
+        // Set URL of the application under test
+        caps.setCapability("app", "bs://<app-id>");
+
+        // Specify device and os_version for testing
+        caps.setCapability("device", "Google Pixel 3");
+        caps.setCapability("os_version", "9.0");
+
+        // Set other BrowserStack capabilities
+        caps.setCapability("project", "First Java Project");
+        caps.setCapability("build", "Java Android");
+        caps.setCapability("name", "first_test");
+
+
+        // Initialise the remote Webdriver using BrowserStack remote URL
+        // and desired capabilities defined above
+        AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(
+                new URL("http://hub.browserstack.com/wd/hub"), caps);
+
+
+        /* Write your Custom code here */
+
+
+        // Invoke driver.quit() after the test is done to indicate that the test is completed.
+        driver.quit();
+
+    }
+
+}
+
