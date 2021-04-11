13756ca1f66b72996af1d8ea1669a68328d6e5c6
MightyBoolik <bkuchaev@gmail.com>
2021-04-11 17:03:25 +0500

    Added AndroidBrowserstackTest

--- src/test/java/tests/AndroidBrowserstackTest.java
+++ src/test/java/tests/AndroidBrowserstackTest.java
@@ -1,17 +1,23 @@
 package tests;
 
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
+@Tag("android")
 public class AndroidBrowserstackTest {
 
     @Test
-    void goolePixelTest(String[] args) throws MalformedURLException, InterruptedException {
+    void mobileTest() throws MalformedURLException, InterruptedException {
 
         DesiredCapabilities caps = new DesiredCapabilities();
 
@@ -20,7 +26,7 @@ public class AndroidBrowserstackTest {
         caps.setCapability("browserstack.key", "xyd9yyz4hnUbpV7QfF7e");
 
         // Set URL of the application under test
-        caps.setCapability("app", "bs://<app-id>");
+        caps.setCapability("app", "bs://c700ce60cf13ae8ed97705a55b8e022f13c5827c");
 
         // Specify device and os_version for testing
         caps.setCapability("device", "Google Pixel 3");
@@ -38,7 +44,20 @@ public class AndroidBrowserstackTest {
                 new URL("http://hub.browserstack.com/wd/hub"), caps);
 
 
-        /* Write your Custom code here */
+        // Test case for the BrowserStack sample Android app.
+        // If you have uploaded your app, update the test case here.
+        AndroidElement searchElement = (AndroidElement) new WebDriverWait(driver, 30).until(
+                ExpectedConditions.elementToBeClickable(
+                        MobileBy.AccessibilityId("Search Wikipedia")));
+        searchElement.click();
+        AndroidElement insertTextElement = (AndroidElement) new WebDriverWait(driver, 30).until(
+                ExpectedConditions.elementToBeClickable(
+                        MobileBy.id("org.wikipedia.alpha:id/search_src_text")));
+        insertTextElement.sendKeys("BrowserStack");
+        Thread.sleep(5000);
+        List<AndroidElement> allProductsName = driver.findElementsByClassName(
+                "android.widget.TextView");
+        assert (allProductsName.size() > 0);
 
 
         // Invoke driver.quit() after the test is done to indicate that the test is completed.
